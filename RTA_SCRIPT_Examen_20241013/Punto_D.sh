3d83ecabdffc682d00d243363dc73e800cf716f0
Tomas Gonzalez <tg17805@gmail.com>
2024-10-13 23:49:43 +0000

    ejercicio D hecho

---
+++ RTA_SCRIPT_Examen_20241013/Punto_D.sh
@@ -0,0 +1,10 @@
+mkdir -p Estructura_Asimetrica/correo Estructura_Asimetrica/clientes
+
+
+touch Estructura_Asimetrica/correo/cartas_{1..100}
+
+
+touch Estructura_Asimetrica/clientes/cartas_{1..100}
+
+
+touch Estructura_Asimetrica/correo/carteros_{1..10}
